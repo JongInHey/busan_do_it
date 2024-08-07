09756f9b63fab4a69ac63c30ba6281895159ed27
JongInHey <youyi1234@naver.com>
2024-08-07 17:38:10 +0900

    header setup-1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,29 @@
+import { routes } from "../routes";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <LOGO>
+        <Link to={routes.home}>MOVIE</Link>
+      </LOGO>
+
+      <Menu>
+        <li>
+          <Link to={"/placelists/0"}>쇼핑</Link>
+        </li>
+        <li>
+          <Link to={"/placelists/1"}>축제</Link>
+        </li>
+        <li>
+          <Link to={"/placelists/2"}>명소</Link>
+        </li>
+      </Menu>
+
+      <SideMenu>
+        <li>
+          <Link to={routes.search}>검색</Link>
+        </li>
+      </SideMenu>
+    </Container>
+  );
 };
